599a18d2ff0d6a5a544024a3cd4571f17b788a38
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-03-06 21:01:55 +0330

    Add : Hover style & Onclick for url navigation

--- components/button/Secondary.vue
+++ components/button/Secondary.vue
@@ -1,9 +1,9 @@
 <template>
     <button
-        class="border px-4 py-2 border-light rounded-sm gap-2 flex items-center justify-center  text-sm text-light cursor-pointer hover:"
+        class="border px-4 py-2 border-light rounded-sm gap-2 flex items-end justify-center text-sm text-light cursor-pointer hover:bg-light/10 transition-colors"
         @click="onClick">
         <span v-if="icon">
-            <Icon :icon="icon" />
+            <Icon :icon="icon" class="text-[18px]" />
         </span>
         <span>{{ label }}</span>
     </button>
@@ -12,12 +12,17 @@
 <script setup>
 import { Icon } from '@iconify/vue/dist/iconify.js';
 
-defineProps({
+const props = defineProps({
     label: String,
     icon: String,
+    url: String,
 });
 
 const onClick = () => {
-    console.log("SecondaryBtn Clicked");
+    if (props.url) {
+        window.open(props.url, '_blank');
+    } else {
+        console.log("No URL provided for this button.");
+    }
 };
 </script>
