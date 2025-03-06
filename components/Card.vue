c9583414b726c4bff28df5048c66782ce1b652e6
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-03-06 16:25:42 +0330

    Implement : Card component

---
+++ components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <nuxt-link :to="route" class="rounded-xl border border-gray p-md">
+        <img class="rounded-md mb-3" :src="image" :alt="name" />
+        <h4 class="text-md text-white">{{ name }}</h4>
+    </nuxt-link>
+</template>
+
+<script setup>
+defineProps({
+    image: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    route: {
+        type: String,
+        required: true
+    }
+});
+</script>
