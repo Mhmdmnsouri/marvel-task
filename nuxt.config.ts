65331c8ebdedb1011a2a31e3487a65eaae2f781a
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-03-06 16:18:16 +0330

    Set : Private/Public key & BaseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    public: {
+      marvelPublicKey: process.env.NUXT_PUBLIC_MARVEL_PUBLIC_KEY,
+      marvelBaseUrl: process.env.NUXT_PUBLIC_MARVEL_BASE_URL,
+    },
+    marvelPrivateKey: process.env.NUXT_MARVEL_PRIVATE_KEY,
+  },
+
   app: {
     head: {
       title: "Marvel",
