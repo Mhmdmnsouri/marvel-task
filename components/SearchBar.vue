8532ec7b1774a5d7c03e328cef7779fef5b923e9
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-03-06 16:26:39 +0330

    Implement : SearchBar component

---
+++ components/SearchBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="search-bar bg-dark rounded-md p-4 flex gap-4 w-full mb-4">
+        <input type="text" placeholder="Search for characters..."
+            class="bg-muted p-2 w-full rounded-sm placeholder-white text-white text-sm focus:ring-0 focus:outline-1 focus:outline-[#666]" />
+        <buttonPrimary label="Search" icon="solar:magnifer-linear" :hideLabelOnMobile="true" />
+    </div>
+</template>
+
+<script setup></script>
