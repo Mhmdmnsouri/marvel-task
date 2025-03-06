4aec3a662e173ad957913ba0f724e263a5e53464
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-03-06 20:58:14 +0330

    Implement : Screen loader

---
+++ components/FullScreenLoader.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="fixed inset-0 z-50 flex items-center justify-center bg-dark bg-opacity-80">
+      <Icon icon="svg-spinners:3-dots-scale" class="text-[60px] text-white" />
+    </div>
+</template>
+  
+  <script setup>
+  import { Icon } from '@iconify/vue'
+  </script>
