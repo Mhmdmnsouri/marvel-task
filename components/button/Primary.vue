494019b611b03c4504554242dd2c7cd18a95b149
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-03-06 16:24:58 +0330

    Implement : Custom btn component

---
+++ components/button/Primary.vue
@@ -0,0 +1,28 @@
+<template>
+    <button
+        class="bg-red rounded-sm gap-2 flex items-center justify-center p-2 text-sm text-white cursor-pointer hover:bg-dark-red transition-colors"
+        @click="onClick">
+        <span :class="{ 'px-1 sm:px-0': hideLabelOnMobile }" v-if="icon">
+            <Icon :icon="icon" />
+        </span>
+
+        <span :class="{ 'hidden sm:inline': hideLabelOnMobile }">{{ label }}</span>
+    </button>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue/dist/iconify.js';
+
+defineProps({
+    label: String,
+    icon: String,
+    hideLabelOnMobile: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const onClick = () => {
+    console.log("PrimaryBtn Clicked");
+};
+</script>
---
+++ components/button/Secondary.vue
@@ -0,0 +1,23 @@
+<template>
+    <button
+        class="border px-4 py-2 border-light rounded-sm gap-2 flex items-center justify-center  text-sm text-light cursor-pointer hover:"
+        @click="onClick">
+        <span v-if="icon">
+            <Icon :icon="icon" />
+        </span>
+        <span>{{ label }}</span>
+    </button>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue/dist/iconify.js';
+
+defineProps({
+    label: String,
+    icon: String,
+});
+
+const onClick = () => {
+    console.log("SecondaryBtn Clicked");
+};
+</script>
