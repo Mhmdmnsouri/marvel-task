753da8db4ad05b1cf2478a4cb833e2ed51c6d554
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-03-07 16:25:32 +0330

    Implement : Loading until show image / Optimize & Lazy loading

--- components/Card.vue
+++ components/Card.vue
@@ -1,11 +1,15 @@
 <template>
     <nuxt-link :to="route"
-        class="group rounded-xl border flex justify-between flex-col border-gray p-md hover:shadow-[0_0px_20px_rgba(255,255,255,0.2)] h-full hover:border-light transition-all">
-        <img class="rounded-md mb-3 w-full h-full" :src="image" :alt="name" />
+        class="group rounded-xl border flex flex-col justify-between border-gray p-md hover:shadow-[0_0px_20px_rgba(255,255,255,0.2)] h-full hover:border-light transition-all">
+        <div class="relative w-full h-full mb-3">
+            <div v-if="!imageLoaded" class="absolute inset-0 flex items-center justify-center">
+                <Icon icon="svg-spinners:6-dots-scale-middle" class="text-[50px] text-light" />
+            </div>
+            <img :src="image" :alt="name" loading="lazy" @load="handleImageLoad"
+                :class="{ invisible: !imageLoaded, 'rounded-md': true, 'w-full': true, 'h-full': true }" />
+        </div>
         <div class="flex items-center">
-            <h4 class="text-md text-white">
-                {{ name }}
-            </h4>
+            <h4 class="text-md text-white">{{ name }}</h4>
             <span
                 class="ml-1 text-white opacity-0 transform -translate-x-2 transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-0">
                 <Icon icon="hugeicons:arrow-right-02" width="24" height="24" />
@@ -15,9 +19,10 @@
 </template>
 
 <script setup>
-import { Icon } from '@iconify/vue/dist/iconify.js';
+import { ref } from 'vue'
+import { Icon } from '@iconify/vue/dist/iconify.js'
 
-defineProps({
+const props = defineProps({
     image: {
         type: String,
         required: true
@@ -30,5 +35,11 @@ defineProps({
         type: String,
         required: true
     }
-});
+})
+
+const imageLoaded = ref(false)
+
+const handleImageLoad = () => {
+    imageLoaded.value = true
+}
 </script>
